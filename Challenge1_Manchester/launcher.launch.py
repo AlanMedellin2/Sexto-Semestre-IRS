ba341e692341e4d7b14102ed85b749410e20e92b
DiegoMBlanco <162824079+DiegoMBlanco@users.noreply.github.com>
2026-02-15 20:05:50 -0600

    Update launcher.launch.py

--- Challenge1_Manchester/launcher.launch.py
+++ Challenge1_Manchester/launcher.launch.py
@@ -1,1 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package = 'Challenge1_Manchester',
+            executable = 'talker_executable',
+            output = 'screen'),
+        Node(
+            package = 'Challenge1_Manchester',
+            executable = 'receiver_executable',
+            output = 'screen')
+    ])
