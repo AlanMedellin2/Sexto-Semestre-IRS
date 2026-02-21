91edacb83c8a22335bfa864e544d4ed66953174e
DiegoMBlanco <162824079+DiegoMBlanco@users.noreply.github.com>
2026-02-21 13:08:13 -0600

    Create launcher.launch.py for motor control nodes
    Add launch file for motor control module with nodes.

--- Challenge2_Manchester/src/motor_control_module/launch/launcher.launch.py
+++ Challenge2_Manchester/src/motor_control_module/launch/launcher.launch.py
@@ -1,1 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package = 'motor_control_module',
+            executable = 'dc_motor',
+            output = 'screen'),
+        Node(
+            package = 'motor_control_module',
+            executable = 'set_point',
+            output = 'screen'),
+        Node(
+            package = 'motor_control_module',
+            executable = 'server',
+            output = 'screen')
+    ])
