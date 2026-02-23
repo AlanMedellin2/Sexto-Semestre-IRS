3511a40b0be4292f97d4b68bfebcce87c71db18e
DiegoMBlanco <162824079+DiegoMBlanco@users.noreply.github.com>
2026-02-23 08:14:20 -0600

    Refactor motor control module and add init subscriber
    Updated motor control logic and added initialization subscriber.

--- Challenge2_Manchester/src/motor_control_module/motor_control_module/controller.py
+++ Challenge2_Manchester/src/motor_control_module/motor_control_module/controller.py
@@ -1,8 +1,7 @@
 import rclpy                #para programar en python
 from rclpy.node import Node #clases nodo
-from std_msgs.msg import Float32 
-from rcl_interfaces.msg import SetParametersResult
-        
+from std_msgs.msg import Float32    
+from custom_interfaces.msg import Init       
 
 class ControleNode(Node):               #definimos la clase
    def __init__(self):
@@ -11,10 +10,10 @@ class ControleNode(Node):               #definimos la clase
     self.declare_parameter('kp', 1.0) #parámetro de ganancia
     self.kp = self.get_parameter('kp'). value
 
-    self.setp = 0
-    self.y = 0 #Salida y del sistema
+    self.setp = 0.0
+    self.y = 0.0 #Salida y del sistema
     self.publisher = self.create_publisher(Float32, 'motor_input_u', 10) # (dato tipo string, 'nombre de tópico', 10 datos se almacenan)
-    timer_period = 0.5
+    timer_period = 0.02
 
     self.setp_sub = self.create_subscription(Float32, 'set_point', self.setp_callback, 10)
     self.y_sub = self.create_subscription(Float32, 'motor_speed_y', self.y_callback, 10)                                       
@@ -22,16 +21,30 @@ class ControleNode(Node):               #definimos la clase
     self.timer = self.create_timer(timer_period, self.timer_cb)     #cada 0.5 segundos se llama al callback
     self.i= 0
 
-    #Parameter Callback
-    self.add_on_set_parameters_callback(self.parameters_callback)
+    #Subscriber init
+    self.init_subscriber = self.create_subscription(
+        Init,
+        '/init_system',
+        self.init_callback,
+        10
+    )
+
+    self.active = False
+
+   def init_callback(self, msg):
+        if msg.info.data == 'resume':
+            self.active = True
+        else:
+            self.active = False
 
    def timer_cb(self):
+    
     msg = Float32()
     error = self.setp - self.y #fórmula oara error
     u = self.kp * error  # u = error, para que pueda ir cambiando respecto a kp
-    msg.data = u
-
-    self.publisher.publish(msg) #Se manda error
+    msg.data = float(u) 
+    if (self.active == True): 
+        self.publisher.publish(msg) #Se manda error
 
     self.i+= 1 #va a aumentar por cada callback
 
@@ -40,20 +53,6 @@ class ControleNode(Node):               #definimos la clase
 
    def y_callback(self, msg):
     self.y = msg.data
-
-   def parameters_callback(self, params):
-        for param in params:
-            #system gain parameter check
-            if param.name == "kp":
-                #check if it is negative
-                if (param.value < 0.0):
-                    self.get_logger().warn("Error en kp, no puede ser negativo")
-                    return SetParametersResult(successful=False, reason="kp no puede ser negativo")
-                else:
-                    self.kp = param.value  # Update internal variable
-                    self.get_logger().info(f"se actualizo el valor de kp a:  {self.kp}")
-
-        return SetParametersResult(successful=True)
 
 
 def main(args=None):
