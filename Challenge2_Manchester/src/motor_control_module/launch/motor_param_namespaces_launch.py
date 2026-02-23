afa749c2bd1ed1364fc59af94404ac85134c02aa
Alan Gustavo Medellin Palacios <a01665056@tec.mx>
2026-02-23 08:22:23 -0600

    Update motor_param_namespaces_launch.py

--- Challenge2_Manchester/src/motor_control_module/launch/motor_param_namespaces_launch.py
+++ Challenge2_Manchester/src/motor_control_module/launch/motor_param_namespaces_launch.py
@@ -1,1 +1,104 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    motor_node_1 = Node(name="motor_sys_1",
+                        package='motor_control',
+                        executable='dc_motor',
+                        emulate_tty=True,
+                        output='screen',
+                        namespace="group1",
+                        parameters=[{
+                            'sample_time': 0.02,
+                            'sys_gain_K': 1.78,
+                            'sys_tau_T': 0.5,
+                            'initial_conditions': 0.0,
+                        }]
+                        )
+
+    sp_node_1 = Node(name="sp_gen_1",
+                     package='motor_control',
+                     executable='set_point',
+                     emulate_tty=True,
+                     output='screen',
+                     namespace="group1"
+                     )
+
+    control_node_1 = Node(name="control_node_1",
+                          package='motor_control',
+                          executable='controlador',
+                          emulate_tty=True,
+                          output='screen',
+                          namespace="group1",
+                          parameters=[{'kp': 2.0}],
+                          )
+
+    motor_node_2 = Node(name="motor_sys_2",
+                        package='motor_control',
+                        executable='dc_motor',
+                        emulate_tty=True,
+                        output='screen',
+                        namespace="group2",
+                        parameters=[{
+                            'sample_time': 0.02,
+                            'sys_gain_K': 1.78,
+                            'sys_tau_T': 0.5,
+                            'initial_conditions': 0.0,
+                        }]
+                        )
+
+    sp_node_2 = Node(name="sp_gen_2",
+                     package='motor_control',
+                     executable='set_point',
+                     emulate_tty=True,
+                     output='screen',
+                     namespace="group2"
+                     )
+
+    control_node_2 = Node(name="control_node_2",
+                          package='motor_control',
+                          executable='controlador',
+                          emulate_tty=True,
+                          output='screen',
+                          namespace="group2",
+                          parameters=[{'kp': 2.0}],
+                          )
+
+    motor_node_3 = Node(name="motor_sys_3",
+                        package='motor_control',
+                        executable='dc_motor',
+                        emulate_tty=True,
+                        output='screen',
+                        namespace="group3",
+                        parameters=[{
+                            'sample_time': 0.02,
+                            'sys_gain_K': 1.78,
+                            'sys_tau_T': 0.5,
+                            'initial_conditions': 0.0,
+                        }]
+                        )
+
+    sp_node_3 = Node(name="sp_gen_3",
+                     package='motor_control',
+                     executable='set_point',
+                     emulate_tty=True,
+                     output='screen',
+                     namespace="group3"
+                     )
+
+    control_node_3 = Node(name="control_node_3",
+                          package='motor_control',
+                          executable='controlador',
+                          emulate_tty=True,
+                          output='screen',
+                          namespace="group3",
+                          parameters=[{'kp': 2.0}],
+                          )
+
+    l_d = LaunchDescription([
+        motor_node_1, sp_node_1, control_node_1,
+        motor_node_2, sp_node_2, control_node_2,
+        motor_node_3, sp_node_3, control_node_3,
+    ])
+  
+    return l_d
