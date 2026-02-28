fed720f8b0ec7006102827b522a35c2fb4376abf
DiegoMBlanco <162824079+DiegoMBlanco@users.noreply.github.com>
2026-02-27 19:10:36 -0600

    Enhance PWM node with publishing functionality
    Refactor PWM publishing logic and update logging message.

--- Challenge3_Manchester/pwm_node.py
+++ Challenge3_Manchester/pwm_node.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 from std_msgs.msg import Float32
+import time as t
 
 #Class Definition
 class PWMPublisher(Node):
@@ -10,8 +11,32 @@ class PWMPublisher(Node):
         super().__init__('pwm_node')
 
 
+        ## Publisher
+        self.publisher = self.create_publisher(Float32, '/cmd_pwm', 10)
 
-        self.get_logger().info("SetPoint Node Started \U0001F680")
+
+
+        self.get_logger().info("PWMPoint Node Started \U0001F680")
+
+
+    def publish_pwm(self):
+        msg = Float32()
+        msg.data = 0.0
+        self.publisher.publish(msg)
+        t.sleep(1)
+        msg.data = 1.0
+        self.publisher.publish(msg)
+        t.sleep(3)
+        msg.data = 0.0
+        self.publisher.publish(msg)
+        t.sleep(2)
+        msg.data = -1.0
+        self.publisher.publish(msg)
+        t.sleep(3)
+        msg.data = 0.0
+        self.publisher.publish(msg)
+        t.sleep(1)
+
 
    
 
@@ -21,14 +46,9 @@ def main(args=None):
     rclpy.init(args=args)
 
     pwm_node = PWMPublisher()
-
-    try:
-        rclpy.spin(pwm_node)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        pwm_node.destroy_node()
-        rclpy.try_shutdown()
+    pwm_node.publish_pwm()
+    pwm_node.destroy_node()
+    rclpy.try_shutdown()
 
 #Execute Node
 if __name__ == '__main__':
