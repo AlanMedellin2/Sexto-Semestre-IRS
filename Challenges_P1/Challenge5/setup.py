9c032d2df65e9ed57cdab75564caf189550cf94d
DiegoMBlanco <162824079+DiegoMBlanco@users.noreply.github.com>
2026-04-23 19:44:30 -0600

    Update setup.py

--- Challenges_P1/Challenge5/setup.py
+++ Challenges_P1/Challenge5/setup.py
@@ -1,1 +1,34 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
 
+package_name = 'motor_control_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),  # ✅ agrega 'launch'
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alan',
+    maintainer_email='alan@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'set_point = motor_control_pkg.set_point:main',
+            'controller = motor_control_pkg.controller:main',
+        ],
+    },
+)
